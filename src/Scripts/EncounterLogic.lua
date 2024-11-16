f97d0926ae927b03ca2c2a17191c76da5d7d3859
NikkelM <57323886+NikkelM@users.noreply.github.com>
2024-11-16 18:04:03 +0000

    Added config option to pause all game timers

--- src/Scripts/EncounterLogic.lua
+++ src/Scripts/EncounterLogic.lua
@@ -1,7 +1,13 @@
 modutil.mod.Path.Wrap("OnAllEnemiesDead", function(base, currentRoom, currentEncounter)
 	-- Pause the timer when combat ends
 	if config.disableTimerOutOfCombat then
-		game.CurrentRun.ModVowOfMoreOrLessTimeTimerPaused = true
+		if config.pauseVowTimerOnly then
+			-- This pauses only the Vow of Time timer
+			game.CurrentRun.ModVowOfMoreOrLessTimeTimerPaused = true
+		else
+			-- This pauses all timers, including the run timer, timed keepsakes and the chaos time curse
+			game.AddTimerBlock(game.CurrentRun, "ModVowOfMoreOrLessTime")
+		end
 	end
 
 	base(currentRoom, currentEncounter)
--- src/Scripts/RoomLogic.lua
+++ src/Scripts/RoomLogic.lua
@@ -1,7 +1,13 @@
 modutil.mod.Path.Wrap("StartRoom", function(base, currentRun, currentRoom)
 	-- Pause the timer when loading the first room in a biome (especially for first room of the run)
 	if currentRoom.BiomeStartRoom then
-		game.CurrentRun.ModVowOfMoreOrLessTimeTimerPaused = true
+		if config.pauseVowTimerOnly then
+			-- This pauses only the Vow of Time timer
+			game.CurrentRun.ModVowOfMoreOrLessTimeTimerPaused = true
+		else
+			-- This pauses all timers, including the run timer, timed keepsakes and the chaos time curse
+			game.AddTimerBlock(game.CurrentRun, "ModVowOfMoreOrLessTime")
+		end
 	end
 
 	base(currentRun, currentRoom)
@@ -10,7 +16,11 @@ end)
 modutil.mod.Path.Wrap("SetupUnit", function(base, unit, currentRun, args)
 	-- Resume the timer when an enemy unit spawns
 	if config.disableTimerOutOfCombat and unit.AddToEnemyTeam then
-		game.CurrentRun.ModVowOfMoreOrLessTimeTimerPaused = false
+		if config.pauseVowTimerOnly then
+			game.CurrentRun.ModVowOfMoreOrLessTimeTimerPaused = false
+		else
+			game.RemoveTimerBlock(game.CurrentRun, "ModVowOfMoreOrLessTime")
+		end
 	end
 
 	base(unit, currentRun, args)
