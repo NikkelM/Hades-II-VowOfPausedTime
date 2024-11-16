8bddb1b9e5f87b78d63c0fc310e8c4cf8f2a60c3
NikkelM <57323886+NikkelM@users.noreply.github.com>
2024-11-16 21:54:10 +0000

    Fixed timer sticking around after a run ends

--- src/Scripts/RunLogic.lua
+++ src/Scripts/RunLogic.lua
@@ -1,13 +1,15 @@
 modutil.mod.Path.Wrap("UpdateTimers", function(base, elapsed)
+	local wasActiveBiomeTimerEnabled = game.CurrentRun.ActiveBiomeTimer
 	-- Quickly toggles the ActiveBiomeTimer off to make sure the time doesn't tick down when it shouldn't
-	if game.CurrentRun.ModVowOfMoreOrLessTimeTimerPaused then
+	if game.CurrentRun.ModVowOfMoreOrLessTimeTimerPaused and wasActiveBiomeTimerEnabled then
 		game.CurrentRun.ActiveBiomeTimer = false
 	end
 
 	base(elapsed)
 
 	-- Re-enable the timer variable to make sure other timers dependent on it can still run (e.g. keepsakes, run timer)
-	if game.CurrentRun.ModVowOfMoreOrLessTimeTimerPaused then
+	-- Only enable it if it was enabled before the wrap, otherwise the timer sticks around after a run ends
+	if game.CurrentRun.ModVowOfMoreOrLessTimeTimerPaused and wasActiveBiomeTimerEnabled then
 		game.CurrentRun.ActiveBiomeTimer = true
 	end
 end)
