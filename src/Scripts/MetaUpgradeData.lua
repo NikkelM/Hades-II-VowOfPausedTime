b4312eaeb7c7dd8ab62b9dd282d7148e71be99ab
NikkelM <57323886+NikkelM@users.noreply.github.com>
2024-11-17 13:18:00 +0000

    Updated config keys

--- src/Scripts/MetaUpgradeData.lua
+++ src/Scripts/MetaUpgradeData.lua
@@ -1,7 +1,7 @@
 local newRanks = {
-	{ Points = 1, ChangeValue = config.timeEasyTier },
-	{ Points = 2, ChangeValue = config.timeMediumTier },
-	{ Points = 3, ChangeValue = config.timeHardTier },
+	{ Points = 1, ChangeValue = config.vowOfTimeEasyTier },
+	{ Points = 2, ChangeValue = config.vowOfTimeMediumTier },
+	{ Points = 3, ChangeValue = config.vowOfTimeHardTier },
 }
 
 game.MetaUpgradeData.BiomeSpeedShrineUpgrade.Ranks = newRanks
--- src/config.lua
+++ src/config.lua
@@ -3,9 +3,9 @@ local config = {
   enabled = true,
   disableVowTimerOutOfCombat = true,
   disableAllTimersOutOfCombat = true,
-  timeEasyTier = 60 * 7.5,
-  timeMediumTier = 60 * 6,
-  timeHardTier = 60 * 4,
+  vowOfTimeEasyTier = 60 * 7.5,
+  vowOfTimeMediumTier = 60 * 6,
+  vowOfTimeHardTier = 60 * 4,
 }
 
 local configDesc = {
@@ -14,11 +14,11 @@ local configDesc = {
   "Whether the timer of the \"Vow of Time\" should be paused outside of combat. The timer will only run when enemies are present (includes roaming enemies in the Fields of Mourning and the Ephyra town square).",
   disableAllTimersOutOfCombat =
   "Whether all game timers (including e.g. the run clear timer, Hermes' keepsake or Chaos' time curse) should be paused out of combat. If false, only the timer of the \"Vow of Time\" will be paused (if the option is enabled).",
-  timeEasyTier =
+  vowOfTimeEasyTier =
   "Time per region on the first (easiest) tier of the \"Vow of Time\", in seconds. The game's default is 9 minutes, the mod lowers this to 7:30 minutes.",
-  timeMediumTier =
+  vowOfTimeMediumTier =
   "Time per region on the second tier of the \"Vow of Time\", in seconds. The game's default is 7 minutes, the mod lowers this to 6 minutes.",
-  timeHardTier =
+  vowOfTimeHardTier =
   "Time per region on the third (most difficult) tier of the \"Vow of Time\", in seconds. The game's default is 5 minutes, the mod lowers this to 4 minutes.",
 }
 
