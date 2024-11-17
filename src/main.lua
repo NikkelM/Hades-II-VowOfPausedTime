7390c084202d5c9e613b69de74c2bdfbd3938ad5
NikkelM <57323886+NikkelM@users.noreply.github.com>
2024-11-17 11:27:04 +0000

    comment out debug function

--- src/main.lua
+++ src/main.lua
@@ -39,23 +39,23 @@ config = chalk.auto 'config.lua'
 public.config = config -- so other mods can access our config
 
 -- For debugging
-function printTable(t, indent)
-	if type(t) ~= "table" then
-		print("Error: Expected a table, got " .. type(t))
-		return
-	end
+-- function printTable(t, indent)
+-- 	if type(t) ~= "table" then
+-- 		print("Error: Expected a table, got " .. type(t))
+-- 		return
+-- 	end
 
-	indent = indent or 0
-	local formatting = string.rep("  ", indent)
-	for k, v in pairs(t) do
-		if type(v) == "table" then
-			print(formatting .. k .. ":")
-			printTable(v, indent + 1)
-		else
-			print(formatting .. k .. ": " .. tostring(v))
-		end
-	end
-end
+-- 	indent = indent or 0
+-- 	local formatting = string.rep("  ", indent)
+-- 	for k, v in pairs(t) do
+-- 		if type(v) == "table" then
+-- 			print(formatting .. k .. ":")
+-- 			printTable(v, indent + 1)
+-- 		else
+-- 			print(formatting .. k .. ": " .. tostring(v))
+-- 		end
+-- 	end
+-- end
 
 local function on_ready()
 	-- what to do when we are ready, but not re-do on reload.
